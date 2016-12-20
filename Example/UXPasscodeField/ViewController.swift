1ad47116b0d7344e2d06760874d4ff70d6ab29b6
Eddie Lau <eddie@touchutility.com>
2016-12-20 13:50:09 +0800

    Improved example

--- Example/UXPasscodeField/ViewController.swift
+++ Example/UXPasscodeField/ViewController.swift
@@ -12,10 +12,12 @@ import UXPasscodeField
 class ViewController: UIViewController {
     
     @IBOutlet weak var passcodeField: UXPasscodeField!
+    @IBOutlet weak var isSecureSwitch: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        isSecureSwitch.isOn = passcodeField.isSecureTextEntry
         passcodeField.becomeFirstResponder()
 
         // Optional, default is false
@@ -27,6 +29,10 @@ class ViewController: UIViewController {
 
     @IBAction func passcodeFieldDidChangeValue() {
         print(passcodeField.passcode)
+    }
+    
+    @IBAction func switchDidChangeValue() {
+        passcodeField.isSecureTextEntry = isSecureSwitch.isOn
     }
 
 }
